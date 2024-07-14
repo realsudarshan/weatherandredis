7460f0fa6874b47837a162e4473627a71f003779
Sudarshan <128029723+realsudarshan@users.noreply.github.com>
2024-07-14 07:14:29 +0545

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   const fetchWeather = async () => {
     try {
-      const response = await axios.get(`http://localhost:5000/api/weather/${city}`);
+      const response = await axios.get(`https://weatherandredis.vercel.app/${city}`);
       console.log(response.data)
       setWeatherData(response.data);
       setError(null);
